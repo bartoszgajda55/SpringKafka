e7cfd990a82a49024ce944958b92052c863481ea
Bartosz Gajda <bg@bartoszgajda.com>
2019-09-30 22:42:53 +0100

    Add clean Spring Boot project

---
+++ src/main/java/com/bartoszgajda/twitchwebhookmanager/TwitchWebhookManagerApplication.java
@@ -0,0 +1,13 @@
+package com.bartoszgajda.twitchwebhookmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitchWebhookManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitchWebhookManagerApplication.class, args);
+	}
+
+}
