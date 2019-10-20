8c0c779b2055b577f4cc7bbb6e5571a984fcfa25
Bartosz Gajda <bg@bartoszgajda.com>
2019-10-20 20:58:31 +0100

    Add the previous state of collection component.

--- src/main/java/com/bartoszgajda/springkafka/SpringKafkaApplication.java
+++ src/main/java/com/bartoszgajda/springkafka/SpringKafkaApplication.java
@@ -1,13 +1,25 @@
 package com.bartoszgajda.springkafka;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.client.WebSocketClient;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 
 @SpringBootApplication
 public class SpringKafkaApplication {
+	private static final String MEETUP_RSVP_ENDPOINT = "ws://stream.meetup.com/2/rsvps";
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringKafkaApplication.class, args);
 	}
 
+	@Bean
+	public ApplicationRunner initializeConnection(CollectionSocketHandler collectionSocketHandler) {
+		return args -> {
+			WebSocketClient socketClient = new StandardWebSocketClient();
+			socketClient.doHandshake(collectionSocketHandler, MEETUP_RSVP_ENDPOINT);
+		};
+	}
 }
