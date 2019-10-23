b67b1dbb94f437e7971508927585b68735cbde46
Bartosz Gajda <bg@bartoszgajda.com>
2019-10-23 21:31:29 +0100

    Add more refined configuration.

---
+++ src/main/java/com/bartoszgajda/springkafka/config/StreamsConfig.java
@@ -0,0 +1,7 @@
+package com.bartoszgajda.springkafka.config;
+
+import org.springframework.cloud.stream.annotation.EnableBinding;
+
+@EnableBinding(RsvpStreams.class)
+public class StreamsConfig {
+}
